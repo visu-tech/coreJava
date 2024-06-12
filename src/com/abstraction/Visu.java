aa72c38e5d0bb76af3882bf2c867f553997736bd
visu-tech <singamvisu29@gmail.com>
2024-06-12 13:55:06 +0100

    added

--- src/com/abstraction/Visu.java
+++ src/com/abstraction/Visu.java
@@ -2,15 +2,13 @@ package com.abstraction;
 
 public class Visu {
     public static void main(String[] args) {
-     String g  = "viswa";
+        String g = "viswa";
         System.out.println(g.charAt(1));
         System.out.println(g.equals("VISWA"));
         System.out.println(g.equalsIgnoreCase("mahesh"));
-        g+="viswa";
+        g += "viswa";
         System.out.println(g);
 
-        StringBuffer  mb = new StringBuffer(1000);
-        mb.capacity();
-        System.out.println(mb);
+        System.out.println("mb");
     }
 }
